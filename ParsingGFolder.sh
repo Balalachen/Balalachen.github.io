0554f018b213ba73e10fdaa2ad25e7956d34dc7f
Yang-Sheng Chen <yeti0193275@gmail.com>
2021-06-18 02:49:52 +0800

    Fixed Typo Error and Exception for empty file name

--- ParsingGFolder.sh
+++ ParsingGFolder.sh
@@ -5,15 +5,18 @@ declare -a NameArray
 curl -L -o ./gfolder "https://drive.google.com/drive/folders/$1?usp=sharing"
 
 cat ./gfolder | grep -Eo 'data-id="[^/"]+' | sed 's/data-id="//' > ./tempIDs
-cat ./gfolder | grep -Eo 'aria-label="[^/"]+$3"' | sed 's/aria-label="//; s/ $3"//' > ./tempNames
+cat ./gfolder | grep -Eo "aria-label=\"[^/\"]+$3\"" | sed "s/aria-label=\"//; s/ $3\"//" > ./tempNames
 
 mapfile -t IDArray < ./tempIDs
 mapfile -t NameArray < ./tempNames
 
 for (( i=0; i<${#IDArray[@]}; i=i+1 ))
 do
-    # echo "Save: ./$2/${NameArray[i]}"
-    curl -L -o "./$2/${NameArray[i]}" "https://drive.google.com/uc?export=download&id=${IDArray[i]}"
+    if [ "${NameArray[i]}" != "" ]
+    then
+        # echo "Save: ./$2/${NameArray[i]}"
+        curl -L -o "./$2/${NameArray[i]}" "https://drive.google.com/uc?export=download&id=${IDArray[i]}"
+    fi
 done
 
 rm ./gfolder
