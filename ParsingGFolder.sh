7fc269fb23ad56d33a97efb0b19b7b42b49f9e74
Yang-Sheng Chen <yeti0193275@gmail.com>
2021-06-18 02:29:14 +0800

    Update Parsing Method, More Extended Usage

--- ParsingGFolder.sh
+++ ParsingGFolder.sh
@@ -5,7 +5,7 @@ declare -a NameArray
 curl -L -o ./gfolder "https://drive.google.com/drive/folders/$1?usp=sharing"
 
 cat ./gfolder | grep -Eo 'data-id="[^/"]+' | sed 's/data-id="//' > ./tempIDs
-cat ./gfolder | grep -Eo 'aria-label="[^/"]+Image"' | sed 's/aria-label="//; s/ Image"//' > ./tempNames
+cat ./gfolder | grep -Eo 'aria-label="[^/"]+$3"' | sed 's/aria-label="//; s/ $3"//' > ./tempNames
 
 mapfile -t IDArray < ./tempIDs
 mapfile -t NameArray < ./tempNames
